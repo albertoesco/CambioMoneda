cbc8b6e0b81c92dbc79b58c7a0070ce223bb72ea
albertoesco <alumno.538326@ies.azarquiel.es>
2024-02-04 11:39:40 +0100

    ExchangeCard

---
+++ comp/ExchangeCard.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { View, Text, StyleSheet,TouchableOpacity } from 'react-native';
+
+const ExchangeCard = ({ exchange, onDelete }) => {
+  if (!exchange) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.card}>
+        <Text style={styles.text}>Origin Currency:</Text>
+        <Text style={styles.content}>{exchange.originCurrency}</Text>
+
+        <Text style={styles.text}>Dest Currency:</Text>
+        <Text style={styles.content}>{exchange.destCurrency}</Text>
+
+        <Text style={styles.text}>Origin Amount:</Text>
+        <Text style={styles.content}>{exchange.originAmount}</Text>
+
+        <Text style={styles.text}>Dest Amount:</Text>
+        <Text style={styles.content}>{exchange.destAmount}</Text>
+      </View>
+
+      <TouchableOpacity onPress={onDelete} style={styles.deleteButton}>
+        <Text style={styles.deleteText}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 5,
+  },
+  card: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  content: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  deleteButton: {
+    backgroundColor: 'red',
+    padding: 10,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  deleteText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
+
+export default ExchangeCard;
